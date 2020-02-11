baa1da49b44eebe07441cc84d0a16de907e93e5b
sanmuchh <sandya.muchharla@publicissapient.com>
2020-02-11 15:01:40 +0000

    Adding report

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,23 +1,57 @@
 package stepDefinitions;
 
+import com.cucumber.listener.Reporter;
+import com.google.common.io.Files;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utilities.DriverBase;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Hooks
 {
 
-    @Before
+    @Before(order = 0)
     public void setup()
     {
         DriverBase.Initialize(System.getProperty("browser"));
     }
 
-    @After
+    @After(order = 0)
     public void quit()
     {
         DriverBase.quit();
+    }
+
+    @Before(order = 1)
+    public void beforeScenario(io.cucumber.core.api.Scenario scenario) {
+        Reporter.assignAuthor("SM - Sandya Muchharla");
+    }
+
+    @After(order = 1)
+    public void afterScenario(io.cucumber.core.api.Scenario scenario) {
+        if (scenario.isFailed()) {
+            String screenshotName = scenario.getName().replaceAll(" ", "_");
+            try {
+                //This takes a screenshot from the driver at save it to the specified location
+                File sourcePath = ((TakesScreenshot) DriverBase.Initialize(System.getProperty("browser"))).getScreenshotAs(OutputType.FILE);
+
+                //Building up the destination path for the screenshot to save
+                //Also make sure to create a folder 'screenshots' with in the cucumber-report folder
+                File destinationPath = new File(System.getProperty("user.dir") + "/target/cucumber/screenshots/" + screenshotName + ".png");
+
+                //Copy taken screenshot from source location to destination location
+                Files.copy(sourcePath, destinationPath);
+
+                //This attach the specified screenshot to the test
+                Reporter.addScreenCaptureFromPath(destinationPath.toString());
+            } catch (IOException e) {
+            }
+        }
     }
 
 }
--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,16 +1,30 @@
 package testRunner;
 
+import com.cucumber.listener.Reporter;
+import dataProviders.FileReaderManager;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+
 @RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/features",
+        glue="stepDefinations",tags="@SmokeTest1",monochrome=true,strict=true,
+        plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/cucumber/report.html"})
 
-@CucumberOptions(monochrome = true,
-        plugin = {"progress", "html:target/cucumber"},
-        features = "src/test/java/features/",
-        glue = {"stepDefinitions"},
-        tags = "@login")
-
-public class TestRunner {
+public class TestRunner
+{
+    //plugin= {"pretty","html:target/cucumber","json:target/cucumber.json","junit:target/cukes.xml"}
+    @AfterClass
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+        Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+        Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        Reporter.setSystemInfo("Machine", "MAC OS" + "64 Bit");
+        Reporter.setSystemInfo("Selenium", "3.14.0");
+        Reporter.setSystemInfo("Maven", "3.6.2");
+        Reporter.setSystemInfo("Java Version", "1.8.0_151");
+    }
 }
